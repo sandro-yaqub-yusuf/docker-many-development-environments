b4c5ea8fd3ba429aae64efea554a1db265bc7a75
sandro-yaqub-yusuf <sandroyy@gmail.com>
2021-01-01 21:26:54 -0300

    Criação do DEV-005
    Criação do ambiente de desenvolvimento 005

--- dev-002/src/public/index.php
+++ dev-002/src/public/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DOCKER - Ambiente de Desenvolvimento 001</title>
+    <title>DOCKER - Ambiente de Desenvolvimento 002</title>
 </head>
 <body>
     <h1>Olá !!!</h1>
